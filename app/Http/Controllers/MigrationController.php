0b0a38c456b305fa7879235c60b147db1163d041
Saddam H. Shovon <saddamhshovon@gmail.com>
2022-04-18 05:06:47 +0600

    Minor fix on MigrationController: table does not exist

--- app/Http/Controllers/MigrationController.php
+++ app/Http/Controllers/MigrationController.php
@@ -9,8 +9,10 @@ class MigrationController extends Controller
 
     public function migrate($user, $pass)
     {
-        if(\Illuminate\Support\Facades\Schema::hasTable('users') && \App\Models\User::where('user_name', env('APP_USER', 'sensible'))->exists()){
-            abort(403);
+        if(\Illuminate\Support\Facades\Schema::hasTable('users')){
+            if(\App\Models\User::where('user_name', env('APP_USER', 'sensible'))->exists()){
+                abort(403);
+            }
         }
         
         if($user != env('APP_USER', 'sensible') || !\Illuminate\Support\Facades\Hash::check($pass, env('MIGRATION_PASSWORD', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'))){
